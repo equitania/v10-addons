bdbad9ec84f79c8f412dcae82cea76ebe613e01d
eqoh <o.hercht@equitania.com>
2017-06-20 13:42:35 +0200

    [ADD] - Neues Modul eq_base - Zusatzfelder für das Unternehmen

---
+++ eq_base/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Modul eq_base
+
+#### 20.06.2017
+#### Version 1.0.0
+##### ADD
+- Neues Modul eq_base
+- Zusatzfelder für das Unternehmen
---
+++ eq_base/models/res_company.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo Addon, Open Source Management Solution
+#    Copyright (C) 2014-now Equitania Software GmbH(<http://www.equitania.de>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from odoo import models, fields, api, _
+
+
+class eq_module_template(models.Model):
+    _inherit = 'res.company'
+
+    eq_ceo_title = fields.Many2one('res.partner.title')
+    eq_ceo_01 = fields.Char('My Char Field')
+    eq_ceo_02 = fields.Char('My Char Field')
+    eq_ceo_03 = fields.Char('My Char Field')
