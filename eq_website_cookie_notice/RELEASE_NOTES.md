c9da5503b5f72d259160822ec7a2c06847a0beb0
Julia Eberle <j.eberle@equitania.de>
2017-07-28 13:33:39 +0200

    [CHG] - Beschreibung angepasst, überflüssige Dateien entfernt

--- eq_website_cookie_notice/RELEASE_NOTES.md
+++ eq_website_cookie_notice/RELEASE_NOTES.md
@@ -1,6 +1,11 @@
 ## Modul eq_website_cookie_notice
 
+#### 28.07.2017
+#### Version 1.0.1
+##### CHG
+- Beschreibung angepasst, überflüssige Dateien entfernt
+
 #### 25.07.2017
 #### Version 1.0.0
 ##### ADD
--neues Modul hnzugefügt; Deutsche Übersetzung / Beschreibung und Styles angepasst
+-neues Modul hinzugefügt; Deutsche Übersetzung / Beschreibung und Styles angepasst
--- eq_website_cookie_notice/__manifest__.py
+++ eq_website_cookie_notice/__manifest__.py
@@ -3,16 +3,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Cookie Notiz',
-    'summary': 'Show cookie notice according to cookie law',
-    'version': '1.0.0',
+    'name': 'Equitania Cookie Notiz Erweiterung',
+    'summary': 'Optische und sprachliche Verbesserungen an der Cookie Notiz',
+    'version': '1.0.1',
     'category': 'Website',
     'author': "Equitania Software GmbH",
 
     'website': 'www.myodoo.de / www.equitania.de',
     'license': 'AGPL-3',
     'depends': [
-        'website_legal_page',
+        'website_legal_page','website_cookie_notice',
     ],
     'data': [
         'templates/website.xml',
