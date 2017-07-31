018d73e9b73211a780caf3ce8b99eb8d6a8fbb8c
Johann Müller <j.mueller@equitania.com>
2017-07-31 18:08:33 +0200

    [IMP] - Checkbox und RadioButtons sind nun 1,5-Fach größer. - Checkboxen in der ersten Spalte einer Tabelle werden mit margin-left um 10 nach rechts verschoben, damit eine bessere Bedienbarkeit gewährleistet bleibt

--- eq_web_responsive/__manifest__.py
+++ eq_web_responsive/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Equitania Web Responsive',
     'license': 'AGPL-3',
-    'version': '1.0.3',
+    'version': '1.0.4',
     'description': """
         Anpassung für das Backend Theme web_responsive
     """,
