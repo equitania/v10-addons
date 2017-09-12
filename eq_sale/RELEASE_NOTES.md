90f43e500a7ad8475945b1dd794932417acf4b2d
Michal Sodomka <=m.sodomka@equitania.com>
2017-09-12 15:40:00 +0200

    [IMP] - Erweiterung um Funktion _eq_display_address

--- eq_sale/RELEASE_NOTES.md
+++ eq_sale/RELEASE_NOTES.md
@@ -1,6 +1,9 @@
 ## Modul eq_sale
 
-
+#### 12.09.2017
+#### Version 1.0.42
+##### IMP
+- Erweiterung um Funktion _eq_display_address
 
 #### 17.08.2017
 #### Version 1.0.41
--- eq_sale/models/res_partner.py
+++ eq_sale/models/res_partner.py
@@ -22,6 +22,18 @@
 
 from odoo import models, fields, api, _
 
+import sys
+import codecs
+sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+sys.stderr = codecs.getwriter('utf8')(sys.stderr)
+
+
+def log(*args):
+    """
+        Kleine Hilfsfunktion, die wir als Logger in der Konsole verwenden
+    """
+    print '#>', args
+
 class res_partner(models.Model):
     _inherit = 'res.partner'
 
@@ -77,13 +89,54 @@ class res_partner(models.Model):
             if tmp_context.get('show_address_only'):
                 name = self._display_address(without_company=True) # record
             if tmp_context.get('show_address'):
-                name = name + "\n" + self._display_address(without_company=True) # record
-            name = name.replace('\n\n','\n')
-            name = name.replace('\n\n','\n')
+                # TODO - führt zu einem Fehler - Expected singleton: res.partner
+                #name = name + "\n" + self._display_address(record, without_company = True)  # record
+                name = name + "\n" + self._eq_display_address(record, without_company=True)
+                log(name)
+
+            #name = name.replace('\n\n','\n')
+            #name = name.replace('\n\n','\n')
             if tmp_context.get('show_email') and record.email:
                 name = "%s <%s>" % (name, record.email)
             res.append((record.id, name))
         return res
+
+
+    @api.multi
+    def _eq_display_address(self, record, without_company = False):
+        '''
+        The purpose of this function is to build and return an address formatted accordingly to the
+        standards of the country where it belongs.
+
+        :param address: browse record of the res.partner to format
+        :returns: the address formatted in a display that fit its country habits (or the default ones
+            if not country is specified)
+        :rtype: string
+        '''
+        # get the information that will be injected into the display format
+        # get the address format
+        self = record
+        address_format = self.country_id.address_format or \
+                         "%(street)s\n%(street2)s\n%(city)s %(state_code)s %(zip)s\n%(country_name)s"
+        args = {
+            'state_code': self.state_id.code or '',
+            'state_name': self.state_id.name or '',
+            'country_code': self.country_id.code or '',
+            'country_name': self.country_id.name or '',
+            'company_name': self.commercial_company_name or '',
+        }
+        for field in self._address_fields():
+            args[field] = getattr(self, field) or ''
+        if without_company:
+            args['company_name'] = ''
+        elif self.commercial_company_name:
+            address_format = '%(company_name)s\n' + address_format
+
+        # Erweiterung der Adresse um HausNr.
+        address_format = address_format.replace('%(street)s', '%(street)s %(eq_house_no)s')
+        print "* address_format: ", address_format
+        print "------------------------------"
+        return address_format % args
 
     @api.model
     def name_search(self, name, args=None, operator='ilike', limit=100):
