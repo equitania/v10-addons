b6e6d46fcda9c54d12c6025ae6ec6735e936a2b3
Robin Prijatel <r.prijatel@equitania.de>
2017-11-07 13:54:18 +0100

    [ADD] - Description angepasst

--- eq_purchase/RELEASE_NOTES.md
+++ eq_purchase/RELEASE_NOTES.md
@@ -1,5 +1,10 @@
 ## Modul eq_purchase
 
+#### 07.11.2017
+#### Version 1.0.24
+##### CHG
+- Description angepasst.
+
 #### 06.11.2017
 #### Version 1.0.23
 ##### CHG
--- eq_purchase/__manifest__.py
+++ eq_purchase/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': "Equitania Einkauf",
     'license': 'AGPL-3',
-    'version': '1.0.23',
+    'version': '1.0.24',
     'category': 'purchase',
     'description': """Extensions for purchase""",
     'author': 'Equitania Software GmbH',
