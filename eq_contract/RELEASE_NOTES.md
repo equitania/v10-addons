a0bab304a26644870fadcf8afef48fba9262b1d7
Robin Prijatel <r.prijatel@equitania.de>
2017-12-22 09:53:27 +0100

    [ADD] - Erweiterung der Veträge Tree View um das Feld 'Date End'

---
+++ eq_contract/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Modul eq_contract
+
+#### 22.12.2017
+#### Version 1.0.0
+##### ADD
+- initialer Commit
+- Erweiterung der Veträge Tree View um das Feld 'Date End'
---
+++ eq_contract/__manifest__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo Addon, Open Source Management Solution
+#    Copyright (C) 2014-now Equitania Software GmbH(<http://www.equitania.de>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Equitania Verträge Erweiterungen',
+    'license': 'AGPL-3',
+    'version': '1.0.0',
+    'description': """
+        Equitania Erweiterungen für das Contract Modul der OCA
+    """,
+    'author': 'Equitania Software GmbH',
+    'website': 'www.myodoo.de',
+    'depends': ['contract'],
+    'category' : 'General Improvements',
+    'summary': 'Equitania Contract Erweiterungen',
+
+    'data': [
+        #"security/ir.model.access.csv"
+        "views/eq_contract_view.xml",
+
+    ],
+    'demo': [],
+    'css': ['base.css'],
+    'installable': True,
+    'auto_install': False,
+}
