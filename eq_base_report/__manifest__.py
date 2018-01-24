9e9ef77bbfd69783f5368f32a75ae61db59bf250
Johann Müller <j.mueller@equitania.com>
2018-01-24 18:15:16 +0100

    [IMP] - Diverse Anpassungen am Header, Footer & Styles:   - Reportfooter für Standard Report Dokumente wird nun durch den Equitania Footer ersetzt   - Reportheader für Standard Reports, welche z.B. noch von Online Website Angeboten angesprochen werden, werden nun durch eine optimierte Version erstzt und beinhalten weiterhin die Adresse der Firma   - Repoststyles haben nun Definitionen für H1 bis h5 in der Schriftgröße

--- eq_base_report/__manifest__.py
+++ eq_base_report/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Equitania Ausdrucke Basis',
     'license': 'AGPL-3',
-    'version': '1.0.26',
+    'version': '1.0.27',
     'description': """
         Allgemeine Anpassungen für die Equitania Reports
     """,
