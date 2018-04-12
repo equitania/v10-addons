d4f7deef2d38cf8a8280668addacc01a56b82bfc
angelina <angelina>
2018-04-12 11:11:51 +0200

    [FIX]- fixing the problem with the empty subject

--- eq_mail_templates/RELEASE_NOTES.md
+++ eq_mail_templates/RELEASE_NOTES.md
@@ -1,6 +1,11 @@
 ## Modul eq_mail_templates
 
 #### 10.04.2018
+#### Version 1.0.3
+##### FIX
+- fixing the problem with the empty subject
+
+#### 10.04.2018
 #### Version 1.0.2
 ##### CHG
 - change descriptions
--- eq_mail_templates/__manifest__.py
+++ eq_mail_templates/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Equitania Mail Templates',
     'license': 'AGPL-3',
-    'version': '1.0.2',
+    'version': '1.0.3',
     'description': """
         Verbesserte Mail Templates für Sales / Purchase / Account Invoice
     """,
