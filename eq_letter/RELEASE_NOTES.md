8aabe9130f98817a5c73b3c9ab898bcc727f2937
Robin Prijatel <r.prijatel@equitania.de>
2018-04-13 08:30:10 +0200

    [ADD] - initial commit

---
+++ eq_letter/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Modul eq_letter
+
+#### 13.04.2018
+#### Version 1.0.0
+##### ADD
+- initial commit
---
+++ eq_letter/__manifest__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo Addon, Open Source Management Solution
+#    Copyright (C) 2014-now Equitania Software GmbH(<http://www.equitania.de>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Equitania Letter',
+    'license': 'AGPL-3',
+    'version': '1.0.0',
+    'description': """
+        Equitania Letter
+    """,
+    'author': 'Equitania Software GmbH',
+    'website': 'www.myodoo.de',
+    'depends': ['base_setup','web','web_planner'],
+    'category' : 'General Improvements',
+    'summary': 'Möglichkeit einen Brief in der Odoo Umgebung zu schreiben',
+
+    'data': [
+        "security/ir.model.access.csv",
+        "views/eq_letter_view.xml",
+        "views/eq_letter_report.xml",
+        "views/eq_report_show.xml",
+
+    ],
+    'demo': [],
+    'css': ['base.css'],
+    'installable': True,
+    'auto_install': False,
+}
