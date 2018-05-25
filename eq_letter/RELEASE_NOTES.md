78f8cdb82a909968e23f82a5e646fc1da185aaf2
Julia Eberle <j.eberle@equitania.com>
2018-05-25 14:00:50 +0200

    [ADD] - module icon for main menu

--- eq_letter/RELEASE_NOTES.md
+++ eq_letter/RELEASE_NOTES.md
@@ -1,5 +1,10 @@
 ## Modul eq_letter
 
+#### 13.05.2018
+#### Version 1.0.1
+##### ADD
+- module icon for main menu
+
 #### 13.04.2018
 #### Version 1.0.0
 ##### ADD
--- eq_letter/__manifest__.py
+++ eq_letter/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Equitania Letter',
     'license': 'AGPL-3',
-    'version': '1.0.0',
+    'version': '1.0.1',
     'description': """
         Equitania Letter
     """,
