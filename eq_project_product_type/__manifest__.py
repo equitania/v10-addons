f62f75796e4f6c1ffbb4560518ec644e4b3ec103
Robin Prijatel <r.prijatel@equitania.de>
2018-09-28 08:03:02 +0200

    [FIX/CHG] change new view for project.task

--- eq_project_product_type/__manifest__.py
+++ eq_project_product_type/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Equitania Project Product Type Extension',
     'license': 'AGPL-3',
-    'version': '1.0.3',
+    'version': '1.0.4',
     'description': """
         Product type Extension for eq_project
     """,
