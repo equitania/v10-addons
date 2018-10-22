4c9f8b9989df2639105750214a92341f669db04b
Robin Prijatel <r.prijatel@equitania.de>
2018-10-22 09:46:38 +0200

    [ADD] - initial commit

---
+++ eq_remove_hr_timesheet/__manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo Addon, Open Source Management Solution
+#    Copyright (C) 2014-now Equitania Software GmbH(<http://www.equitania.de>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Equitania Remove HR Timesheet',
+    'license': 'AGPL-3',
+    'version': '1.0.0',
+    'description': """
+        Equitania Remove HR Timesheet
+    """,
+    'author': 'Equitania Software GmbH',
+    'website': 'www.myodoo.de',
+    'depends': ['base_setup','hr_timesheet'],
+    'category' : 'General Improvements',
+    'summary': 'This module makes the HR Timesheet menuitem invisible',
+
+    'data': [
+        "views/templates.xml",
+
+    ],
+    'demo': [],
+    'css': ['base.css'],
+    'installable': True,
+    'auto_install': False,
+}
