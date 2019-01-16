581cb23b13b8bfbb404eb6e47e011d713f226a88
Johann Müller <j.mueller@equitania.de>
2019-01-16 15:43:32 +0100

    [CHG] New Module and backend Icon

--- eq_letter/RELEASE_NOTES.md
+++ eq_letter/RELEASE_NOTES.md
@@ -1,5 +1,10 @@
 ## Modul eq_letter
 
+#### 16.01.2019
+#### Version 1.0.2
+##### CHG
+- New Module and backend Icon
+
 #### 13.05.2018
 #### Version 1.0.1
 ##### ADD
--- eq_letter/__manifest__.py
+++ eq_letter/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Equitania Letter',
     'license': 'AGPL-3',
-    'version': '1.0.1',
+    'version': '1.0.2',
     'description': """
         Equitania Letter
     """,
