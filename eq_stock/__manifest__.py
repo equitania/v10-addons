7a8ca15454e05113b6ddc1f046975794ed985559
Robin Prijatel <r.prijatel@equitania.de>
2019-01-22 14:13:51 +0100

    [FIX] - FIX odoo standard in action_assign from stock_move when an ancestor exists and product type is consu. Then check if the state of ancestor is done, if yes continue with move_assign.

--- eq_stock/__manifest__.py
+++ eq_stock/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Equitania Lager Optimierungen',
     'license': 'AGPL-3',
-    'version': '1.0.27',
+    'version': '1.0.28',
     'description': """
         Erweiterung für Lager
     """,
