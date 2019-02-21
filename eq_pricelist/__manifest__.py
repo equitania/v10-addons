efed57d485cfc3e9aa24de62802d3ce2d1613b35
Michal Sodomka <m.sodomka@equitania.de>
2019-02-21 11:12:41 +0100

    [ADD] - added new colum with sequence and link to price_list defined here https://gitlab.ownerp.io/v10-myodoo-public/v10-addons/issues/34

--- eq_pricelist/__manifest__.py
+++ eq_pricelist/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': "Equitania Preislisten Optimierungen",
     'license': 'AGPL-3',
-    'version': '1.0.5',
+    'version': '1.0.6',
     'category': 'pricelist',
     'description': """ Extension for pricelist-positions """,
     'author': 'Equitania Software GmbH',
