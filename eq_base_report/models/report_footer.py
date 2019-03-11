29e668ba4d14660a872148cf4254eb61cc0f5a17
Martin Schmid <m.schmid@equitania.de>
2019-03-11 17:38:29 +0100

    Update eq_base_report/eq_res_config.py, eq_base_report/eq_res_company.py, eq_base_report/__manifest__.py, eq_base_report/__init__.py, eq_base_report/models/__init__.py, eq_base_report/models/eq_report_helper.py, eq_base_report/models/report_footer.py files

--- eq_base_report/models/report_footer.py
+++ eq_base_report/models/report_footer.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# Copyright Equitania Software GmbH - Germany - https://www.equitania.de
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, models, _
 from odoo.exceptions import ValidationError
