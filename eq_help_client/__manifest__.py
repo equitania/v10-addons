5b39752dd66a6f7e374cc4f96c70de689bbae213
Martin Schmid <m.schmid@equitania.de>
2019-03-11 17:33:48 +0100

    Update templates/eq_module_template/__manifest__.py, templates/eq_module_template/__init__.py, templates/eq_module_template/models/__init__.py, templates/eq_module_template/models/module.py, eq_help_client/__manifest__.py files

--- eq_help_client/__manifest__.py
+++ eq_help_client/__manifest__.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+# Copyright Equitania Software GmbH - Germany - https://www.equitania.de
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 {
     'name': 'MyOdoo Help Client',
     'summary': """
