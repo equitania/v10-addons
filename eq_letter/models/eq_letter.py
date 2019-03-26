823441645472edaa947b35f395e20b75ccc4ef10
Angelina Anakievska <a.anakievska@ownerp.net>
2019-03-26 14:00:32 +0100

    [CHG] - Clean up code eq_letter

--- eq_letter/models/eq_letter.py
+++ eq_letter/models/eq_letter.py
@@ -1,38 +1,27 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Odoo Addon, Open Source Management Solution
-#    Copyright (C) 2014-now Equitania Software GmbH(<http://www.equitania.de>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright Equitania Software GmbH - Germany - https://www.equitania.de
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api, _
+from odoo import models, fields
 
-class eq_letter(models.Model):
+class EqLetter(models.Model):
+    """
+    Class EqLetter
+    """
     _name = 'eq_letter'
     _rec_name = 'eq_letter_subject'
 
     eq_text = fields.Html('Text', required=True)
 
     def get_current_user(self):
-        obj = self.env['res.users'].search([('id','=',self._uid)])
+        """
+        Default function which return value for eq_letter_form field
+        :return: partner id
+        """
+        obj = self.env['res.users'].search([('id', '=', self._uid)])
         return obj.partner_id
 
-    eq_letter_from = fields.Many2one('res.partner', string='From', default=get_current_user, required=True)
+    eq_letter_from = fields.Many2one(
+        'res.partner', string='From', default=get_current_user, required=True)
     eq_letter_subject = fields.Char('Subject', required=False)
     eq_letter_to = fields.Many2one('res.partner', string="To", required=True)
-
-
