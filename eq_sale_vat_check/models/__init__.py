1b24928313f8ee7d05172d337a7f5db6ffeebdcc
Angelina Anakievska <a.anakievska@ownerp.net>
2019-03-27 15:34:13 +0100

    Update all *.py files in eq_sale_vat_check

--- eq_sale_vat_check/models/__init__.py
+++ eq_sale_vat_check/models/__init__.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014-now Equitania Software GmbH - Pforzheim - Germany
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from . import account_fiscal_position
 from . import res_partner
 from . import sale_order
