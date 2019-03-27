912f0156d73b62c41f6c95d7b0df2f8500ff8fd8
Angelina Anakievska <a.anakievska@ownerp.net>
2019-03-27 15:45:29 +0100

    Update all *.py files in eq_timesheet_invoice_no_sep_lines

--- eq_timesheet_invoice_no_sep_lines/models/models.py
+++ eq_timesheet_invoice_no_sep_lines/models/models.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014-now Equitania Software GmbH - Pforzheim - Germany
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 import time
 
 from odoo import _, api, fields, models
