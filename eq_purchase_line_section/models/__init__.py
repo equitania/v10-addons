43c381a9c61990950e28de7973562442a40426cf
Angelina Anakievska <a.anakievska@ownerp.net>
2019-03-27 15:01:42 +0100

    Update all *.py files in eq_purchase_line_section

--- eq_purchase_line_section/models/__init__.py
+++ eq_purchase_line_section/models/__init__.py
@@ -1,2 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014-now Equitania Software GmbH - Pforzheim - Germany
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from . import purchase_order_line
 from . import purchase_order
--- eq_purchase_line_section/models/purchase_order.py
+++ eq_purchase_line_section/models/purchase_order.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014-now Equitania Software GmbH - Pforzheim - Germany
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from odoo import models, fields, api, _
 from itertools import groupby
 
