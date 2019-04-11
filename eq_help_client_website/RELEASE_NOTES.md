274c3fd3e9280e4bb564237d0703735a3772fc38
Michal Sodomka <m.sodomka@equitania.de>
2019-04-11 08:38:41 +0200

    [ADD] initial commit of eq_help_client_website

---
+++ eq_help_client_website/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Modul eq_help_client_website
+
+#### 11.04.2019
+#### Version 1.0.0
+##### ADD
+- initial commit of eq_help_client_website
---
+++ eq_help_client_website/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014-now Equitania Software GmbH - Pforzheim - Germany
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'MyOdoo Help Client for Frontend',
+    'summary': """
+        MyOdoo Help Client for Frontend will Create Buttons for Models which will request Help form the Help Server
+        """,
+    'version': '1.0.0',
+    'author': "Callino, improved by Equitania Software GmbH",
+    'maintainer': 'Callino, improved by Equitania Software GmbH',
+    'website': 'https://equitania.atlassian.net/wiki/spaces/MH/overview',
+    'license': 'AGPL-3',
+    'category': 'Documentation',
+    'depends': [
+        'website', 'website_sale'
+    ],
+    'data': [
+        'views/assets.xml',
+        'views/eq_help_client_frontend.xml',
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'auto_install': True
+}
